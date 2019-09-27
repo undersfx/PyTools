ab932297be96d173ab95bb3f8a7b87188960489a
undersfx <atchin_@hotmail.com>
2019-09-27 17:03:59 -0300

    Adiciona requirements e linters

--- libpythonpro/github_api.py
+++ libpythonpro/github_api.py
@@ -1,17 +1,18 @@
 import requests
 
 
-def buscar_usuario(username):
+def buscar_usuario(username: str) -> dict:
     """
     Busca perfil de um usuario no Github
 
     :param username: str com o nome de usuario no github
-    :return: json com os dados do perfil do usuario
+    :return: dict com os dados do perfil do usuario
     """
 
     url = f'https://api.github.com/users/{username}'
     r = requests.get(url)
     return r.json()
+
 
 if __name__ == '__main__':
     user = buscar_usuario('undersfx')
