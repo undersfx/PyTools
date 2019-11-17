7980e870a37288b2072d1db5b6d0c7621909609f
undersfx <atchin_@hotmail.com>
2019-11-17 13:25:32 -0300

    Adiciona testes de usuarios

---
+++ libpythonpro/tests/tests_spam/test_usuarios.py
@@ -0,0 +1,21 @@
+def test_salvar_usuario():
+    conexao = Conexao()
+    sessao = conexao.gerar_sessao()
+    usuario = Usuario(nome='Renzo')
+    sessao.salvar(usuario)
+    assert isinstance(usuario.id, int)
+    sessao.rollback()
+    sessao.fechar()
+    conexao.fechar()
+
+
+def test_listar_usuario():
+    conexao = Conexao()
+    sessao = conexao.gerar_sessao()
+    usuarios = [Usuario(nome='Renzo'), Usuario(nome='Luciano')]
+    for usuario in usuarios:
+        sessao.salvar(usuario)
+    assert usuarios == sessao.listar()
+    sessao.rollback()
+    sessao.fechar()
+    conexao.fechar()
