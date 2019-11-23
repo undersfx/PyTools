1d16908a2007d308a1e22f7a308a35b6b2179b15
undersfx <atchin_@hotmail.com>
2019-11-23 17:24:59 -0300

    Setup do Conftest

--- libpythonpro/tests/tests_spam/test_usuarios.py
+++ libpythonpro/tests/tests_spam/test_usuarios.py
@@ -1,23 +1,4 @@
-import pytest
-from libpythonpro.spam.db import Conexao
 from libpythonpro.spam.modelos import Usuario
-
-
-@pytest.fixture(scope='session')
-def conexao():
-    conexao_obj = Conexao()
-    yield conexao_obj
-    conexao_obj.fechar()
-
-
-@pytest.fixture
-def sessao(conexao):
-    # Setup
-    sessao_obj = conexao.gerar_sessao()
-    yield sessao_obj
-    # Tear Down
-    sessao_obj.rollback()
-    sessao_obj.fechar()
 
 
 def test_salvar_usuario(sessao):
