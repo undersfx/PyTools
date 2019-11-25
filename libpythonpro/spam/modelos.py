b3015380823e1f6f680d54e0e1bd474a3cf07f65
undersfx <atchin_@hotmail.com>
2019-11-25 14:28:57 -0300

    Produção de Código Testável

--- libpythonpro/spam/modelos.py
+++ libpythonpro/spam/modelos.py
@@ -1,4 +1,5 @@
 class Usuario:
-    def __init__(self, nome):
+    def __init__(self, nome, email):
         self.nome = nome
+        self.email = email
         self.id = None
--- libpythonpro/tests/tests_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro/tests/tests_spam/test_envio_para_base_de_usuarios.py
@@ -1,5 +1,7 @@
+import pytest
 from libpythonpro.spam.enviador_de_email import Enviador
 from libpythonpro.spam.main import EnviadorDeSpam
+from libpythonpro.spam.modelos import Usuario
 
 
 def test_envio_de_spam(sessao):
@@ -9,3 +11,28 @@ def test_envio_de_spam(sessao):
         'Curso Python Pro',
         'Novos Módulos'
     )
+
+
+@pytest.mark.parametrize(
+    'usuarios',
+    [
+        [
+            Usuario(nome='Renzo', email='renzo@python.pro.br'),
+            Usuario(nome='Luciano', email='luciano@python.pro.br')
+        ],
+        [
+            Usuario(nome='Renzo', email='renzo@python.pro.br')
+        ]
+    ]
+    )
+def test_quantidade_de_spam(sessao, usuarios):
+    for usuario in usuarios:
+        sessao.salvar(usuario)
+    enviador = Enviador()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
+    enviador_de_spam.enviar_emails(
+        'renzo@python.pro.br',
+        'Curso Python Pro',
+        'Novos Módulos'
+    )
+    assert len(usuarios) == enviador.quantidade_emails_enviados
