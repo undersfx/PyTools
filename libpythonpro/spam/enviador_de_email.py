58a3c034f7d176f92dfd0f11f230afe4ec46e65a
undersfx <atchin_@hotmail.com>
2020-02-06 20:03:54 -0300

    aula: Injeção de Dependências

--- libpythonpro/spam/enviador_de_email.py
+++ libpythonpro/spam/enviador_de_email.py
@@ -1,11 +1,7 @@
 class Enviador:
-    def __init__(self):
-        self.quantidade_emails_enviados = 0
-
     def enviar(self, remetente, distinatario, assunto, corpo):
         if '@' not in remetente:
             raise EmailInvalido(f'Email remetente {remetente} inválido.')
-        self.quantidade_emails_enviados += 1
         return remetente
 
 
