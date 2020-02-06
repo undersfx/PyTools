a8e0b7c45a706b2ce7f941f869e52b1525861588
undersfx <atchin_@hotmail.com>
2020-02-06 20:03:54 -0300

    aula: Injeção de Dependências

--- libpythonpro/tests/tests_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro/tests/tests_spam/test_envio_para_base_de_usuarios.py
@@ -4,6 +4,18 @@ from libpythonpro.spam.main import EnviadorDeSpam
 from libpythonpro.spam.modelos import Usuario
 
 
+class EnviadorMock(Enviador):
+    def __init__(self):
+        super().__init__()
+        self.quantidade_emails_enviados = 0
+        self.parametros_de_envio = None
+
+    def enviar(self, remetente, distinatario, assunto, corpo):
+        self.quantidade_emails_enviados += 1
+        self.parametros_de_envio = (remetente, distinatario, assunto, corpo)
+        return remetente
+
+
 def test_envio_de_spam(sessao):
     enviador_de_spam = EnviadorDeSpam(sessao, Enviador())
     enviador_de_spam.enviar_emails(
@@ -28,7 +40,7 @@ def test_envio_de_spam(sessao):
 def test_quantidade_de_spam(sessao, usuarios):
     for usuario in usuarios:
         sessao.salvar(usuario)
-    enviador = Enviador()
+    enviador = EnviadorMock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'renzo@python.pro.br',
@@ -36,3 +48,19 @@ def test_quantidade_de_spam(sessao, usuarios):
         'Novos Módulos'
     )
     assert len(usuarios) == enviador.quantidade_emails_enviados
+
+
+def test_parametros_de_spam(sessao):
+    usuario = Usuario(nome='Renzo', email='renzo@python.pro.br')
+    sessao.salvar(usuario)
+    enviador = EnviadorMock()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
+    enviador_de_spam.enviar_emails(
+        'renzo@python.pro.br',
+        'Curso Python Pro',
+        'Novos Módulos'
+    )
+    assert enviador.parametros_de_envio == ('renzo@python.pro.br',
+                                            'renzo@python.pro.br',
+                                            'Curso Python Pro',
+                                            'Novos Módulos')
