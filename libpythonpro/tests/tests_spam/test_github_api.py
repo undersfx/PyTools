3bd3bd69560c6a7313c06a81c3230fab2ffdc40d
undersfx <atchin_@hotmail.com>
2020-02-07 17:09:33 -0300

    aula: Isolamento de Imports

--- libpythonpro/tests/tests_spam/test_github_api.py
+++ libpythonpro/tests/tests_spam/test_github_api.py
@@ -1,9 +1,21 @@
+import pytest
 from libpythonpro import github_api
 from unittest.mock import Mock
 
-def test_buscar_avatar():
+@pytest.fixture
+def avatar_url():
     resp_mock = Mock()
-    resp_mock.json.return_value = {'login':'undersfx', 'avatar_url': 'https://avatars2.githubusercontent.com/u/7119450?v=4'}
+    url = 'https://avatars2.githubusercontent.com/u/7119450?v=4'
+    resp_mock.json.return_value = {'login':'undersfx', 'avatar_url': url}
+    request_original = github_api.requests.get
     github_api.requests.get = Mock(return_value=resp_mock)
+    yield url
+    github_api.requests.get = request_original
+
+def test_buscar_avatar(avatar_url):
     url = github_api.buscar_usuario('undersfx')
-    assert 'https://avatars2.githubusercontent.com/u/7119450?v=4' == url
+    assert avatar_url == url
+
+def test_buscar_avatar_integracao():
+    url = github_api.buscar_usuario('renzon')
+    assert 'https://avatars3.githubusercontent.com/u/3457115?v=4' == url
