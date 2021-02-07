7470a05310dfc3ed777fa28e32539651530e8fe4
undersfx <atchin_@hotmail.com>
2021-02-07 16:07:44 -0300

    fix: text against new API link

--- libpythonpro/tests/tests_spam/test_github_api.py
+++ libpythonpro/tests/tests_spam/test_github_api.py
@@ -7,8 +7,8 @@ from unittest.mock import Mock
 def avatar_url(mocker):
     # Criação do objeto resposta (Mock da resposta da API)
     resp_mock = Mock()
-    url = 'https://avatars2.githubusercontent.com/u/7119450?v=4'
-    resp_mock.json.return_value = {'login': 'undersfx', 'avatar_url': url}
+    url = 'https://avatars.githubusercontent.com/u/3457115?v=4'
+    resp_mock.json.return_value = {'login': 'renzon', 'avatar_url': url}
 
     # request_original = github_api.requests.get
     # github_api.requests.get = Mock(return_value=resp_mock)
@@ -28,4 +28,4 @@ def test_buscar_avatar(avatar_url):
 
 def test_buscar_avatar_integracao():
     url = github_api.buscar_usuario('renzon')
-    assert 'https://avatars3.githubusercontent.com/u/3457115?v=4' == url
+    assert 'https://avatars.githubusercontent.com/u/3457115?v=4' == url
